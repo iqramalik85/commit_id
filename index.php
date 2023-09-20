7b557be6cc84ae6f3c173185b16642b3c51a31d7
iqramalik85 <malikayra85@gmail.com>
2023-09-20 17:12:52 +0500

    track APM changes trough commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>NewReic deployment php application</title>
+</head>
+<body>
+    <h1>Hello, PHP!</h1>
+    <?php
+    echo "This is my test  PHP web application for NewRelic change tracking.";
+    ?>
+</body>
+</html>
